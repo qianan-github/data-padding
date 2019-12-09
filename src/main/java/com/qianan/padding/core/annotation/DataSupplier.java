b6290852e2bc4df77d099d258feb375480719bf1
xiaolin40310904 <xiaolin40310904@163.com>
2019-12-09 11:33:54 +0800

    @DataSupplier去掉@Component

--- src/main/java/com/qianan/padding/core/annotation/DataSupplier.java
+++ src/main/java/com/qianan/padding/core/annotation/DataSupplier.java
@@ -1,12 +1,10 @@
 package com.qianan.padding.core.annotation;
 
-import org.springframework.stereotype.Component;
 
 import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
 public @interface DataSupplier {
 }
